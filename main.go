188875a28e76e8755a509f35f891bff9c3dc4668
Ihor Bondar <yfafyz2910@gmail.com>
2022-06-14 15:46:40 +0300

    Added homework file

--- main.go
+++ main.go
@@ -2,32 +2,24 @@ package main
 
 import "fmt"
 
-
-const pi = 3.1415
-
 func main() {
 
-	printRadiusCircle()
-	Rectangle()
-		
-} 
-		func printRadiusCircle()  {
+	var ApplePrice float64 = 5.99
+	var PearPrice int = 7
+	var OurBudget int = 23
 
-			radius := 4
+	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? ", ApplePrice * float64(9),  PearPrice * 8)
 
-			circleArea:= float32(radius) * float32(radius) * pi
+	fmt.Println("2. Скільки груш ми можемо купити?", OurBudget / PearPrice)
+	
+	fmt.Println("3. Скільки яблук ми можемо купити?", float64(OurBudget) / 	ApplePrice)
 
-			fmt.Println("Радиус круга:", circleArea, "\n")
-		}
+	if float64(OurBudget) > ApplePrice * float64(2) + float64(PearPrice) * float64(2) {
 
+		fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?", "Да, можем")
+	} else {
+		fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?", "Нет,не можем")
+	}
 
-		func Rectangle() {
-			x := 10
-			y := 5
+}
 
-			SquareRectangle := x * y
-			PerimetrRectangle := 2 * (x + y)
-
-			fmt.Println("Площадь прямоугольника: ", SquareRectangle)
-			fmt.Println("Периметр прямоугольника: ", PerimetrRectangle)
-		}
